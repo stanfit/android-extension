39097f1156248b04897bd44bcecf7f2e6bc99114
Matsushita Kohei <ad.special008@gmail.com>
2020-07-17 20:41:59 +0900

    Fix view extension

--- extension/src/main/java/com/stanfit/android/extension/library/view/View+Click.kt
+++ extension/src/main/java/com/stanfit/android/extension/library/view/View+Click.kt
@@ -18,12 +18,27 @@ fun View.pauseClickTimer(delayMillis: Long = 500) {
  *
  * @param block Unit
  */
-inline fun View.setOnClickPauseListener(crossinline block: (View) -> Unit) {
-    if (hasOnClickListeners()) {
-        return
-    }
+inline fun View.setOnClickPauseListener(
+    crossinline block: (View) -> Unit
+) {
     setOnClickListener {
         pauseClickTimer(500)
+        block.invoke(it)
+    }
+}
+
+/**
+ * Click listeners to disable clicking on the View for a certain amount of time in order to prevent continuous hitting.
+ *
+ * @param delayMillis Click disablement time (in milliseconds)
+ * @param block Unit
+ */
+inline fun View.setOnClickPauseListener(
+    delayMillis: Long,
+    crossinline block: (View) -> Unit
+) {
+    setOnClickListener {
+        pauseClickTimer(delayMillis)
         block.invoke(it)
     }
 }
