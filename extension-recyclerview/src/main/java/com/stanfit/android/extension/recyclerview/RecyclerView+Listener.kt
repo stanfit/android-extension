ec2e046492003eb5004a8d62c911947597d4f523
Matsushita Kohei <ad.special008@gmail.com>
2020-07-27 20:25:44 +0900

    Fix recycler view listener extension

--- extension-recyclerview/src/main/java/com/stanfit/android/extension/recyclerview/RecyclerView+Listener.kt
+++ extension-recyclerview/src/main/java/com/stanfit/android/extension/recyclerview/RecyclerView+Listener.kt
@@ -1,24 +1,118 @@
 package com.stanfit.android.extension.recyclerview
 
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 
 /**
- * Set the listener to change the position of the item.
+ * Set the listener when position of the first item is changed.
  *
  * @param bloc Position Change Listener.
  */
-inline fun RecyclerView.setOnItemPositionChangeListener(crossinline bloc: (Int) -> Unit) {
+fun RecyclerView.setFindFirstVisibleItemPositionChangeListener(bloc: (Int) -> Unit) {
     val manager = layoutManager ?: throw IllegalStateException("not found layout manager.")
-    if (manager is LinearLayoutManager) {
-        addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-                super.onScrollStateChanged(recyclerView, newState)
-                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
-                    val position = manager.findFirstVisibleItemPosition()
-                    bloc(position)
+    when (manager) {
+        is LinearLayoutManager -> {
+            addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                    super.onScrollStateChanged(recyclerView, newState)
+                    if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                        val position = manager.findFirstVisibleItemPosition()
+                        bloc(position)
+                    }
                 }
-            }
-        })
+            })
+        }
+        is GridLayoutManager -> {
+            addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                    super.onScrollStateChanged(recyclerView, newState)
+                    if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                        val position = manager.findFirstVisibleItemPosition()
+                        bloc(position)
+                    }
+                }
+            })
+        }
+        is StaggeredGridLayoutManager -> {
+            addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                    super.onScrollStateChanged(recyclerView, newState)
+                    if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                        val position = manager.findFirstVisibleItemPositions(null)
+                        bloc(getFirstVisibleItem(position))
+                    }
+                }
+            })
+        }
     }
+}
+
+/**
+ * Set the listener when position of the last item is changed.
+ *
+ * @param bloc Position Change Listener.
+ */
+fun RecyclerView.setFindLastVisibleItemPositionChangeListener(bloc: (Int) -> Unit) {
+    val manager = layoutManager ?: throw IllegalStateException("not found layout manager.")
+    when (manager) {
+        is LinearLayoutManager -> {
+            addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                    super.onScrollStateChanged(recyclerView, newState)
+                    if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                        val position = manager.findLastVisibleItemPosition()
+                        bloc(position)
+                    }
+                }
+            })
+        }
+        is GridLayoutManager -> {
+            addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                    super.onScrollStateChanged(recyclerView, newState)
+                    if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                        val position = manager.findLastVisibleItemPosition()
+                        bloc(position)
+                    }
+                }
+            })
+        }
+        is StaggeredGridLayoutManager -> {
+            addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                    super.onScrollStateChanged(recyclerView, newState)
+                    if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                        val position = manager.findLastVisibleItemPositions(null)
+                        bloc(getLastVisibleItem(position))
+                    }
+                }
+            })
+        }
+    }
+}
+
+private fun getFirstVisibleItem(firstVisibleItemPositions: IntArray): Int {
+    var maxSize = 0
+    for (i in firstVisibleItemPositions.indices) {
+        if (i == 0) {
+            maxSize = firstVisibleItemPositions[i]
+        } else if (firstVisibleItemPositions[i] > maxSize) {
+            maxSize = firstVisibleItemPositions[i]
+        }
+    }
+    return maxSize
+}
+
+private fun getLastVisibleItem(lastVisibleItemPositions: IntArray): Int {
+    var maxSize = 0
+    for (i in lastVisibleItemPositions.indices) {
+        if (i == 0) {
+            maxSize = lastVisibleItemPositions[i]
+        } else if (lastVisibleItemPositions[i] > maxSize) {
+            maxSize = lastVisibleItemPositions[i]
+        }
+    }
+    return maxSize
 }
