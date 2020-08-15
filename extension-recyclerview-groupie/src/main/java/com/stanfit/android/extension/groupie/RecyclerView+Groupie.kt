bb1665fa804958d11040d6775bc1cf57df93eaba
Matsushita Kohei <ad.special008@gmail.com>
2020-08-15 09:21:06 +0900

    Add recyclerview groupie extension

---
+++ extension-recyclerview-groupie/src/main/java/com/stanfit/android/extension/groupie/RecyclerView+Groupie.kt
@@ -0,0 +1,54 @@
+package com.stanfit.android.extension.groupie
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.xwray.groupie.Group
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+
+/**
+ * Adapter of groupie
+ */
+val groupAdapter = GroupAdapter<GroupieViewHolder>()
+
+/**
+ * Get adapter of groupie
+ *
+ * @return GroupAdapter
+ */
+val RecyclerView.groupieAdapter: GroupAdapter<*>
+    get() {
+        val adapter = adapter
+        if (adapter is GroupAdapter) {
+            return adapter
+        }
+        throw IllegalAccessException("don't have adapter.")
+    }
+
+/**
+ * Set up a click listener for an item that has been set up as a Groupie adapter
+ *
+ * @param block Block
+ */
+inline fun <reified T : Group> RecyclerView.setGroupieOnItemClickListener(crossinline block: (T, View) -> Unit) {
+    val adapter = adapter
+    if (adapter is GroupAdapter) {
+        adapter.setOnItemClickListener { item, view ->
+            if (item is T) block(item, view)
+        }
+    }
+}
+
+/**
+ * Set up a long click listener for an item that has been set up as a Groupie adapter
+ *
+ * @param block Block
+ */
+inline fun <reified T : Group> RecyclerView.setGroupieOnItemLongClickListener(crossinline block: (T, View) -> Boolean) {
+    val adapter = adapter
+    if (adapter is GroupAdapter) {
+        adapter.setOnItemLongClickListener { item, view ->
+            return@setOnItemLongClickListener if (item is T) block(item, view) else false
+        }
+    }
+}
