d188377ea090777a5d591fc72d19b5b32fddd6e3
Matsushita Kohei <ad.special008@gmail.com>
2021-06-15 12:14:39 +0900

    fix scroll listener to view from scrollview

--- extension/src/main/java/com/stanfit/android/extension/library/view/View+Scroll.kt
+++ extension/src/main/java/com/stanfit/android/extension/library/view/View+Scroll.kt
@@ -37,3 +37,77 @@ fun View.isScrollLeft(): Boolean {
 fun View.isScrollRight(): Boolean {
     return !canScrollHorizontally(-1)
 }
+
+/**
+ * Set scroll state listener.
+ *
+ * @param bloc callback.
+ */
+inline fun View.setOnScrollStateListener(crossinline bloc: (ScrollState) -> Unit) {
+    setOnScrollChangeListener { _, scrollX, scrollY, oldScrollX, oldScrollY ->
+        // scroll y
+        if (scrollY != oldScrollY) {
+            when {
+                // scroll to top
+                scrollY < oldScrollY -> {
+                    bloc(ScrollState.ScrollTop(scrollY, oldScrollY))
+                }
+                // scroll to bottom
+                scrollY > oldScrollY -> {
+                    bloc(ScrollState.ScrollBottom(scrollY, oldScrollY))
+                }
+            }
+        }
+        // scroll x
+        if (scrollX != oldScrollX) {
+            when {
+                // scroll to right
+                scrollX > oldScrollX -> {
+                    bloc(ScrollState.ScrollRight(scrollX, oldScrollX))
+                }
+                // scroll to left
+                scrollX < oldScrollX -> {
+                    bloc(ScrollState.ScrollLeft(scrollX, oldScrollX))
+                }
+            }
+        }
+    }
+}
+
+/**
+ * Scroll state.
+ */
+sealed class ScrollState {
+
+    /**
+     * Scroll to top.
+     *
+     * @property scrollY new position.
+     * @property oldScrollY old position.
+     */
+    data class ScrollTop(val scrollY: Int, val oldScrollY: Int) : ScrollState()
+
+    /**
+     * Scroll to bottom.
+     *
+     * @property scrollY new position.
+     * @property oldScrollY old position.
+     */
+    data class ScrollBottom(val scrollY: Int, val oldScrollY: Int) : ScrollState()
+
+    /**
+     * Scroll to right.
+     *
+     * @property scrollX new position.
+     * @property oldScrollX old position.
+     */
+    data class ScrollRight(val scrollX: Int, val oldScrollX: Int) : ScrollState()
+
+    /**
+     * Scroll to left.
+     *
+     * @property scrollX new position.
+     * @property oldScrollX old position.
+     */
+    data class ScrollLeft(val scrollX: Int, val oldScrollX: Int) : ScrollState()
+}
